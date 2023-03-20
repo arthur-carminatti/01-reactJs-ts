bcd6f792f17fb46704995648bee9484662b1b1b4
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-03-20 14:42:49 -0300

    Adding typescript to the project

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css'
+
+import './global.css';
+
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/arthur-carminatti.png',
+      name: 'Arthur Carminatti',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Hey guys 👋', },
+      { type: 'paragraph', content: 'This is my first ReactJS project 🚀' },
+      { type: 'link', content: 'https://github.com/arthur-carminatti' },
+    ],
+    publishedAt: new Date("2023-03-09 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Hey guys 👋', },
+      { type: 'paragraph', content: 'This is my first ReactJS project 🚀' },
+      { type: 'link', content: 'https://github.com/arthur-carminatti' },
+    ],
+    publishedAt: new Date("2023-03-10 20:00:00"),
+  }
+]
+
+
+export function App() {
+  return (
+    <div>
+
+      <Header />
+
+      <div className={styles.wrapper}>
+
+        <Sidebar />
+
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
